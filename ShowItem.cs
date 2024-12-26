d470412d69bba74fb6075278d66276cda6debfd2
Marshall <benson.marshall@gmail.com>
2024-12-25 18:02:34 -0800

    updated venue table

--- ShowItem.cs
+++ ShowItem.cs
@@ -7,7 +7,8 @@
         public string? BAND { get; set; }
         public DateTime? DATE { get; set; }
         public string? VENUE { get; set; }
-        public string? STREET1 { get; set; }
+        public string? STREET { get; set; }
+        public string? CITY { get; set; }
         public string? STATE { get; set; }
         public int? ZIP { get; set; }
         public string? COUNTRY { get; set; }
